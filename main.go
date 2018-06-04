10a41a406f029b9c0fa5a3d0d47cf669628b7438
Naofumi MURATA <naofumi_murata@voyagegroup.com>
2018-06-05 00:02:11 +0900

    gofmt -w main.go

--- main.go
+++ main.go
@@ -3,27 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/deanishe/awgo"
 	"log"
 	"net/http"
 	"net/url"
 	"strings"
-	"github.com/deanishe/awgo"
-	"github.com/PuerkitoBio/goquery"
 )
 
-var	(
+var (
 	wf         *aw.Workflow
 	helpURL    = "https://github.com/nao23/alfred-thesession-workflow"
 	maxResults = 10
 )
 
-const BASE_URL   = "http://thesession.org"
+const baseURL = "http://thesession.org"
 
 func GetHTML(target string, keyword string) *goquery.Document {
 	// Request the HTML page.
 	values := url.Values{}
 	values.Add("q", keyword)
-	res, err := http.Get(BASE_URL + "/" + target + "/search?" + values.Encode())
+	res, err := http.Get(baseURL + "/" + target + "/search?" + values.Encode())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -45,7 +45,7 @@ func SearchTunes(target string, keyword string) {
 		titleAnchor := s.Find("a.manifest-item-title")
 		titleStr := fmt.Sprintf("%d. %s %s", i+1, titleAnchor.Text(), titleAnchor.Next().Text())
 		href, _ := titleAnchor.Attr("href")
-		href = BASE_URL + href
+		href = baseURL + href
 		wf.NewItem(titleStr).Subtitle(href).Arg(href).Valid(true)
 	})
 }
@@ -56,7 +56,7 @@ func SearchRecordings(target string, keyword string) {
 		titleAnchor := s.Find("a.manifest-item-title")
 		titleStr := fmt.Sprintf("%d. %s by %s", i+1, titleAnchor.Text(), titleAnchor.Next().Text())
 		href, _ := titleAnchor.Attr("href")
-		href = BASE_URL + href
+		href = baseURL + href
 		wf.NewItem(titleStr).Subtitle(href).Arg(href).Valid(true)
 	})
 }
@@ -67,7 +67,7 @@ func SearchSessions(target string, keyword string) {
 		titleAnchor := s.Find("a.manifest-item-title")
 		titleStr := fmt.Sprintf("%d. %s", i+1, strings.Trim(titleAnchor.Parent().Text(), "\n"))
 		href, _ := titleAnchor.Attr("href")
-		href = BASE_URL + href
+		href = baseURL + href
 		wf.NewItem(titleStr).Subtitle(href).Arg(href).Valid(true)
 	})
 }
@@ -78,21 +78,24 @@ func init() {
 
 func run() {
 	// Parse cmd-line arguments
-	target  := flag.String("target", "tunes", "search target")
+	target := flag.String("target", "tunes", "search target")
 	keyword := flag.String("keyword", "Kesh", "keyword")
 	flag.Parse()
 
 	switch *target {
-	case "tunes": SearchTunes(*target, *keyword)
-	case "recordings": SearchRecordings(*target, *keyword)
-	case "sessions": SearchSessions(*target, *keyword)
+	case "tunes":
+		SearchTunes(*target, *keyword)
+	case "recordings":
+		SearchRecordings(*target, *keyword)
+	case "sessions":
+		SearchSessions(*target, *keyword)
 	default:
 	}
 
-	wf.WarnEmpty("No matching " + *target, "Try a different keyword?")
+	wf.WarnEmpty("No matching "+(*target), "Try a different keyword?")
 	wf.SendFeedback()
 }
 
-func main()  {
+func main() {
 	aw.Run(run)
 }
